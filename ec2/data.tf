0bb9eac7b96926565a7f4260cfde6d4917408537
sharanya chandu <you@example.com>
2023-06-03 13:10:07 +0530

    auto save

--- ec2/data.tf
+++ ec2/data.tf
@@ -1,7 +1,15 @@
-variable "new"{
+variable "new" {
     default = "This is Terraform concept"
 }
 
-output "output1"{
+output "output1" {
     value = var.new
+}
+
+variable "sample2" {
+    default = "hello terraform"
+}
+
+output "sample_op" {
+    value = var.sample2
 }
