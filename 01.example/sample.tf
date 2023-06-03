bcbad8199e600d3fefe3fd32f764b000a9e82c07
sharanya chandu <you@example.com>
2023-06-03 13:26:41 +0530

    auto save

--- 01.example/sample.tf
+++ 01.example/sample.tf
@@ -1,7 +1,16 @@
-variable = "new"{
+variable = "new" {
     default = "This is terraform concept"
 }
 
-output = "new_op"{
+output  "new_op"{
     value = var.new
+}
+
+
+variable "sample2" {
+    default = "hello terraform"
+}
+
+output "sample_op" {
+    value = var.sample2
 }
